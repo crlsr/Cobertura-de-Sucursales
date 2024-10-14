735cf4fd578be413117dd9d0631f54f4ce355437
crlsr <r.carlos@correo.unimet.edu.ve>
2024-10-14 18:43:16 -0400

    JSON DataGetter Commit

--- src/JSON/LecturaJSON.java
+++ src/JSON/LecturaJSON.java
@@ -3,17 +3,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package JSON;
-import org.json.JSONObject;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.JOptionPane;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 
-/**
- *
- * @author pseba
- */
-public class LecturaJSON {
+
+public class LecturaJSON { 
+    private JSONObject data;
+
+    /*
+        Constructor de LecturaJSON:
+            Recibe como parametro un archivo al cual llamaremos endpoint para poder acceder al archivo JSON
+            Luego hacemos un try-with-resourses pasandole como resourse el archivo con el JSON (así en caso de que haya un
+                error en la lectura del archivo, maneje mejor la excepción y lo cierre al terminar de extraer sus datos).
+            Posteriormente definimos nuestra data la cual será el archivo JSON convertido en un Objeto del tipo JSONObject operable denteo de Jsva
+            Por ultimo validamos cualquier error mostrando que hibieron errores al internar abrir el archivo en cuestión.
     
-     
+            NOTA: Se le pasa al JSONObject un JSONTokener para poder acceder a la informacion del archivo como string y pasarlo aformato JSONObject.
+    */
+    public LecturaJSON(File endpoint) {
+        try(FileReader reader = new FileReader(endpoint)){
+            this.data = new JSONObject(new JSONTokener(reader));
+        } catch(IOException e){
+            JOptionPane.showMessageDialog(null, "Error al imntentar abrir: " + e.getMessage());
+        }
+        
+    }
+    
+    //Método para poder acceder al información de nuestro JSON
+    public JSONObject getData() {
+        return data;
+    }
 }
--- src/proyectocoberturasucursales/ProyectoCoberturaSucursales.java
+++ src/proyectocoberturasucursales/ProyectoCoberturaSucursales.java
@@ -4,8 +4,12 @@
  */
 package proyectocoberturasucursales;
 import EDD.Grafo;
+import JSON.LecturaJSON;
 import Objetos.Estacion;
 import EDD.ListaAdyacencia;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 /**
  *
  * @author pseba
@@ -16,7 +20,29 @@ public class ProyectoCoberturaSucursales {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Esquema a seguir para implementar añadido del JSON en la Interfaz
+        
+        /*
+        Esquema:
+            En este esquema se cumplen los pasos de mostrar ventana para buscar el archivo JSON en cuestión usando un filtrador
+            para solo aceptar archivos JSON por parte del usuario aplicando el filtro, luego se recibe la respuesta de la selección
+            de archivos la cual puede ser valida o no valida (no se selecciono archivos), luego de ello se procede a obtener el archivo
+            crear el objeto para la lectura del JSON y moestra por consola el contenido del JSON para poder percatarnos que la importación de 
+            información del JSON es correcta.
+        */
+        LecturaJSON object;
+        JFileChooser finder = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos JSON", "json");
+        finder.setFileFilter(filter);
+        int response = finder.showOpenDialog(null);
+        
+        if(response == JFileChooser.APPROVE_OPTION){
+            File selected = finder.getSelectedFile();
+            object = new LecturaJSON(selected);
+            System.out.println(object.getData().toString(2));
+        }
+        
+       //Pruebas Pedro 
         Grafo nuevo = new Grafo();
         Estacion x = new Estacion("La hoyada");
         Estacion y = new Estacion("Manao");
@@ -38,18 +64,7 @@ public class ProyectoCoberturaSucursales {
         System.out.println(nuevo.getListavertices().getVfirst().getTinfo().getNombre());
         System.out.println(lista.print());
         nuevo.mostrarGrafo();
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+   
     }
     
 }
